addd3d639ee3d809b790808c36661034ae596d85
SleepyReaperVK <vadkud123@gmail.com>
2023-12-03 10:47:21 +0200

    Working autocomplete , need to store localy Data

--- weatherapi/src/App.js
+++ weatherapi/src/App.js
@@ -4,23 +4,17 @@ import { useSelector } from 'react-redux';
 import Navbar from './components/Navbar';
 import WeatherPage from './pages/WeatherPage';
 import FavoritesPage from './pages/FavoritesPage';
-import Axios from "axios";
+
 
 function App() {
   const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
-  const fetchDATA = (DATA) => {
-    Axios.get(`MY_API/${DATA}`).then(
-      (res) => {
-        
-      }
-    );
-  };
+  document.body.classList.toggle('dark-theme', isDarkTheme);
 
-  // Use useEffect to apply styles when isDarkTheme changes
+ 
   useEffect(() => {
     console.log('isDarkTheme changed:', isDarkTheme);
-    document.body.classList.toggle('dark-theme', isDarkTheme);
-  }, [isDarkTheme]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
--- weatherapi/src/pages/WeatherPage.js
+++ weatherapi/src/pages/WeatherPage.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function WeatherPage() {
   const [city, setCity] = useState('');
+  const [cityList, setCityList] = useState([]);
+
+  const fetchAutoComplete = async (city) => {
+    try {
+      const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=6Ihvh9Hhy84ZKD0ujd2VNgEkWmH7GeGu&q=${city}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    if (city.length >= 2) {
+      const fetchData = async () => {
+        const data = await fetchAutoComplete(city);
+        setCityList(data);
+      };
+      console.log('fetching')
+      fetchData();
+    }
+  }, [city]);
+
+
 
   const handleSearch = () => {
     // Add logic to fetch weather data for the city
@@ -12,6 +37,16 @@ function WeatherPage() {
     // Add logic to save the city as a favorite
   };
 
+  const handleDropdownChange = (e) => {
+    const selectedCityKey = e.target.value;
+  
+    // Now you can access both the key and other properties from selectedCity
+    console.log('Selected key:', selectedCityKey);
+ 
+    
+    // Update the city input and setCityList with the selected city
+    setCity(`${selectedCityKey}`);
+  };
   return (
     <Container>
       <Row className="mt-5">
@@ -23,19 +58,23 @@ function WeatherPage() {
               type="text"
               placeholder="Enter city"
               value={city}
-              onChange={(e) => setCity(e.target.value)}
+              onChange={(e)=> setCity(e.target.value)}
             />
+            {city.length > 0 && (
+              <Form.Control as="select" multiple onChange={handleDropdownChange}>
+                {cityList.map((item) => (
+                  <option key={item.key} value={item.key}>
+                    {`${item.AdministrativeArea.LocalizedName} , ${item.Country.LocalizedName}`}
+                  </option>
+                ))}
+              </Form.Control>
+            )}
           </Form.Group>
-          <Button variant="primary" onClick={handleSearch}>
-            Search
-          </Button>{' '}
-          <Button variant="success" onClick={handleSave}>
-            Save as Favorite
-          </Button>
         </Col>
+        <p>{city}</p>
       </Row>
     </Container>
   );
-}
+};
 
 export default WeatherPage;
